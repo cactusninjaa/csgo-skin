ae275a26ed1c822545497d9685f67c1ec95e4174
cactusninjaa <thomas.sauvage@edu-devinci.fr>
2024-12-08 22:11:45 +0100

    EDIT structure

--- pages/api/question.js
+++ pages/api/question.js
@@ -1,5 +1,16 @@
-import { getStoredSkins } from './skin.js';
+import { getStoredSkins } from '../../utils/skinsService';
+let skins = [];
 
+export const getStoredSkins = async () => {
+  if (!skins.length) {
+    throw new Error('No skins available');
+  }
+  return skins;
+};
+
+export const setSkins = async (newSkins) => {
+  skins = newSkins;
+};
 let dailyQuestions = {
   date: null,
   questions: []
@@ -15,19 +26,17 @@ const generateDailyQuestions = async () => {
   return results;
 };
 
-const getQuestions = async (req, res) => {
+export default async function handler(req, res) {
   try {
-    const currentDate = new Date().toDateString(); // Obtenir la date en cours
+    const currentDate = new Date().toDateString();
+
     if (dailyQuestions.date !== currentDate) {
-      // Si la date a changé, régénérer les questions
       dailyQuestions.date = currentDate;
       dailyQuestions.questions = await generateDailyQuestions();
     }
-    
-    res.json(dailyQuestions.questions);
+
+    res.status(200).json(dailyQuestions.questions);
   } catch (error) {
     res.status(500).json({ error: "An error occurred" });
   }
-};
-
-export { getQuestions };
+}
