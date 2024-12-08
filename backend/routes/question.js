1b4dd6c639e261bdda0feb22286468c0a15d0b4f
cactusninjaa <thomas.sauvage@edu-devinci.fr>
2024-12-08 23:28:42 +0100

    EDIT link api

--- backend/routes/question.js
+++ backend/routes/question.js
@@ -1,23 +1,33 @@
-import {getStoredSkins} from './skin.js'
+import { getStoredSkins } from './skin.js';
 
-export const getQuestions = async (req, res) => {
-    try {
-        const skins = await getStoredSkins();
+let dailyQuestions = {
+  date: null,
+  questions: []
+};
 
-        const id = parseInt(req.params.id); 
-        if (isNaN(id) || id <= 0) {
-            return res.status(400).json({ error: "Invalid ID provided" });
-        }
-        if (skins.length === 0) {
-            return res.status(404).json({ error: "No skins available" });
-        }
-        const results = []; 
-        for (let i = 1; i <= id; i++) {
-            const randomIndex = Math.floor(Math.random() * skins.length);
-            results.push(skins[randomIndex]); 
-        }
-        res.json(results);
-    } catch (error) {
-        res.status(500).json({ error: "An error occurred" });
+const generateDailyQuestions = async () => {
+  const skins = await getStoredSkins();
+  const results = [];
+  for (let i = 0; i < 5; i++) {
+    const randomIndex = Math.floor(Math.random() * skins.length);
+    results.push(skins[randomIndex]);
+  }
+  return results;
+};
+
+const getQuestions = async (req, res) => {
+  try {
+    const currentDate = new Date().toDateString(); // Obtenir la date en cours
+    if (dailyQuestions.date !== currentDate) {
+      // Si la date a changé, régénérer les questions
+      dailyQuestions.date = currentDate;
+      dailyQuestions.questions = await generateDailyQuestions();
     }
-}
+    
+    res.json(dailyQuestions.questions);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
+  }
+};
+
+export { getQuestions };
